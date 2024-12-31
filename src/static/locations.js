8e106f4f4033d96b736f2061ad96af9ab5cf72dd
Your Name <akshay.moryani@appinventiv.com>
2024-12-31 11:47:42 +0530

    locationsadded

--- src/static/locations.js
+++ src/static/locations.js
@@ -1,92 +1,183 @@
-locations=[
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+locations = [
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.606167",
-      "riderLongitude": "77.362380"
-    },
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.229533",
+      "riderLongitude": "55.384823"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.606082",
-      "riderLongitude": "77.363404"
-    },
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.229894",
+      "riderLongitude": "55.383700"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.606016",
-      "riderLongitude": "77.366783"
-    },
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.230268",
+      "riderLongitude": "55.382755"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.607826",
-      "riderLongitude": "77.372633"
-    },
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.231030",
+      "riderLongitude": "55.382858"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.607810",
-      "riderLongitude": "77.372880"
-    },
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.230903",
+      "riderLongitude": "55.382844"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.606588",
-      "riderLongitude": "77.372964"
-    },
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.233533",
+      "riderLongitude": "55.384056"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.606435",
-      "riderLongitude": "77.373591"
-    },
-    {
-      "storeLocationLat": "28.606167",
-      "storeLocationLng": "77.362380",
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.234647",
+      "riderLongitude": "55.382415"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
       "speed": "0.72037643",
       "accuracy": "22.084",
       "heading": 101,
-      "deliveryLocationLat": "28.606068",
-      "deliveryLocationLng": "77.374336",
-      "riderLatitude": "28.606068",
-      "riderLongitude": "77.374336"
-    }
-  ]
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.236331",
+      "riderLongitude": "55.379002"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.237633",
+      "riderLongitude": "55.376144"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.238844",
+      "riderLongitude": "55.373233"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.238888",
+      "riderLongitude": "55.365553"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.235783",
+      "riderLongitude": "55.361210"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.234038",
+      "riderLongitude": "55.358194"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.235268",
+      "riderLongitude": "55.355312"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.235448",
+      "riderLongitude": "55.354591"
+  },
+  {
+      "storeLocationLat": "25.265486",
+      "storeLocationLng": "55.319909",
+      "speed": "0.72037643",
+      "accuracy": "22.084",
+      "heading": 101,
+      "deliveryLocationLat": "25.234285",
+      "deliveryLocationLng": "55.354926",
+      "riderLatitude": "25.235116",
+      "riderLongitude": "55.355116"
+  }
+]
+
+
+console.log(locations.length);
   
 module.exports=locations
