4025c2b903b1e05972e10e761cc6ca7d7e38f11e
Your Name <akshay.moryani@appinventiv.com>
2025-01-10 13:13:47 +0530

    tender

--- src/static/service.js
+++ src/static/service.js
@@ -74,6 +74,7 @@ async function publishData(brand, data) {
       const now = new Date().toISOString();
       let lastCoordinatesUpdated = now;
       let payloadUpdateTime = now;
+      let trackingId;
 
       if (existingDoc.exists()) {
         const existingData = existingDoc.data();
@@ -108,6 +109,16 @@ async function publishData(brand, data) {
         }
 
         payloadUpdateTime = now;
+
+
+        // persisting data
+        if(existingData["trackId"]){
+          trackingId=existingData.trackId;
+        }
+        else{
+          trackingId=""
+        }
+        
       }
 
      
@@ -133,9 +144,10 @@ async function publishData(brand, data) {
         ...data,
        lastCoordinatesUpdated,
         payloadUpdateTime,
+        trackId:trackingId
       };
       // Add or overwrite the document in Firestore
-      await setDoc(docRef, updatedData, { merge: true });
+      await setDoc(docRef, updatedData);
 
       console.log("Document added with ID:", docId);
       resolve(`Data added successfully with ID: ${docId}`);
